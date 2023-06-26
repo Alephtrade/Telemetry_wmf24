cf0eb8d1154a75ca8963f4f076bee8f1df5df2db
dimon <dimonstalkers@mail.ru>
2023-06-26 15:10:03 +0300

    update status

--- check_machine_status.py
+++ check_machine_status.py
@@ -24,16 +24,18 @@ d = db_driver.get_last_downtime()
 d_id, d_date_start, d_date_end, d_status = r
 logging.info(f'last_stat_record: {r}')
 
+if status == 1:
+    if d and d_date_end is None:
+        db_driver.update_downtime(d_id, datetime.datetime.now())
+elif status == 0:
+    if d and d_date_end is None:
+        db_driver.create_downtime(datetime.datetime.now(), 0)
 if r:
     last_id, end_time = r
 if status == 1 and (end_time is None and last_id is not None):
-    if d and d_date_end is None:
-        db_driver.update_downtime(d_id, datetime.datetime.now())
     logging.info(f'status is 1 and last_id is {last_id}, calling close_error_code_by_id({last_id})')
     db_driver.close_error_code_by_id(last_id)
 elif status == 0 and (end_time is not None or last_id is None):
-    if d and d_date_end is None:
-        db_driver.create_downtime(datetime.datetime.now(), 0)
     logging.info(f'status is 0 and end_time is {end_time}, calling create_error_record(-1)')
     db_driver.create_error_record('-1', 'Кофемашина недоступна')
 
