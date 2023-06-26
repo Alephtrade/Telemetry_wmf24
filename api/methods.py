757ae87caebac5241fd0c7428bb56df2a9a77acd
dimon <dimonstalkers@mail.ru>
2023-06-26 10:33:21 +0300

    update getBeverageStatistics

--- api/methods.py
+++ api/methods.py
@@ -41,8 +41,20 @@ def getBeverageStatistics():
     ws.send(request)
     received_data = ws.recv()
     logging.info(f"COFFEE_MACHINE: Received {received_data}")
+    part_number = get_part_number_local()
+    logging.info(f"COFFEE_MACHINE: Received {part_number}")
     text_file = open("response.txt", "a")
     text_file.write(received_data)
+    received_data = received_data.replace(']', '', 1)
+    received_data = received_data + ', {"device_id" : ' + str(part_number) + '}]'
+    # received_data.append({"device_id": part_number})
+    # r = requests.post('https://wmf24.ru/api/servicestatistics', json=received_data)
+    url = "https://wmf24.ru/api/beveragestatistics"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=received_data)
+    logging.info(f"WMFMachineStatConnector: GET response: {response.text}")
     ws.close()
     return True
 
