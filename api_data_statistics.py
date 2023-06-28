1f67d0dc97d1553ce34e89135afd2e3b2e5238e9
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-06-28 13:35:44 +0300

    upload cron agent on data statistics controller

---
+++ api_data_statistics.py
@@ -0,0 +1,3 @@
+from api.methods import send_report_info
+
+r = send_report_info()
--- api_starter.py
+++ api_starter.py
@@ -1,5 +1,6 @@
-from api.methods import getServiceStatistics, getBeverageStatistics
+from api.methods import getServiceStatistics, getBeverageStatistics, send_report_info
 
 q = getServiceStatistics()
 w = getBeverageStatistics()
 #e = startpushdispensingfinished()
+r = send_report_info()
