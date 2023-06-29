3930e3e92135e94b75f9e0d90682897cc8747b6d
dimon <dimonstalkers@mail.ru>
2023-06-29 11:19:54 +0300

    update beverages

--- api/methods.py
+++ api/methods.py
@@ -80,7 +80,7 @@ def getBeverageStatistics():
                 summ += item2
                 recipes.append(item)
 
-    create_record = db_conn.create_beverages_log(device_code, summ, datetime.now(), 0, date_formed, recipes)
+    create_record = db_conn.create_beverages_log(device_code, summ, datetime.now(), 0, date_formed, json.dumps(recipes))
     #url = "https://wmf24.ru/api/beveragestatistics"
     #headers = {
     #    'Content-Type': 'application/json'
