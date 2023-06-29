ab41ced688ae2e66f716efa26c6aa641d6f04026
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-06-29 18:02:58 +0300

    update beverages

--- beverages_worker.py
+++ beverages_worker.py
@@ -5,9 +5,9 @@ from db.models import WMFSQLDriver
 from datetime import datetime, timedelta
 
 db_driver = WMFSQLDriver()
-last_send = db_driver.get_last_beverages_log()[4]
+last_send = db_driver.get_last_beverages_log()
 prev_time_formed = datetime.strptime(last_send, "%Y-%m-%d %H:%M:%S")
-if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp())) > prev_time_formed or last_send is None:
+if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp())) <= prev_time_formed or last_send[4] is None :
     get = creator.Take_Create_Beverage_Statistics()
 
 print(WMFSQLDriver.get_not_sended_beverages_log())
