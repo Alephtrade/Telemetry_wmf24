57c4f685266ee776deda879bc0217d25446ebc1a
dimon <dimonstalkers@mail.ru>
2023-06-29 13:12:56 +0300

    update beverages

--- api/beverages/main.py
+++ api/beverages/main.py
@@ -1,5 +1,5 @@
-from creator import Take_Create_Beverage_Statistics
-from sender import Send_Statistics
+import creator
+import sender
 import sys
 sys.path.append("../../")
 from db.models import WMFSQLDriver
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 last_send = WMFSQLDriver.get_last_beverages_log()
 prev_time_formed = datetime.strptime(last_send[4], "%Y-%m-%d %H:%M:%S")
 if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp())) > prev_time_formed:
-    get = Take_Create_Beverage_Statistics()
+    get = creator.Take_Create_Beverage_Statistics()
 
 print(WMFSQLDriver.get_not_sended_beverages_log())
 #send = Send_Statistics()
