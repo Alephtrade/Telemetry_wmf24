503d64e306ac66200f47bde61601633cc19ec1fb
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-06-30 12:44:16 +0300

    upload cron agent on data statistics controller

--- beverages_worker.py
+++ beverages_worker.py
@@ -5,16 +5,22 @@ from db.models import WMFSQLDriver
 from datetime import datetime, timedelta
 
 db_driver = WMFSQLDriver()
-last_send = db_driver.get_last_beverages_log()
-if last_send is not None:
-    date_formed = last_send[4]
-    prev_time_formed = datetime.strptime(date_formed, "%Y-%m-%d %H:%M:%S")
-    if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=1)).timestamp())) >= prev_time_formed:
+
+def are_need_to_create():
+    last_send = db_driver.get_last_beverages_log()
+    if last_send is not None:
+        date_formed = last_send[4]
+        prev_time_formed = datetime.strptime(date_formed, "%Y-%m-%d %H:%M:%S")
+        if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=1)).timestamp())) >= prev_time_formed:
+            get = creator.Take_Create_Beverage_Statistics()
+            print(get)
+    else:
         get = creator.Take_Create_Beverage_Statistics()
         print(get)
-else:
-    get = creator.Take_Create_Beverage_Statistics()
-    print(get)
 
-print(db_driver.get_not_sended_beverages_log())
+def get_reports_and_send_or_nothing():
+    return db_driver.get_not_sended_beverages_log()
+
+#are_need_to_create()
+print(get_reports_and_send_or_nothing())
 #send = Send_Statistics()
