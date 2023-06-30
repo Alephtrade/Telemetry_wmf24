9f1c3890a665a52f4da596fdb7619e5eb870d392
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-06-30 13:32:43 +0300

    upload cron agent on data statistics controller

--- api/beverages/sender.py
+++ api/beverages/sender.py
@@ -9,10 +9,12 @@ DEFAULT_WMF_PARAMS = settings.DEFAULT_WMF_PARAMS
 db_conn = WMFSQLDriver()
 
 def Send_Statistics(data):
+    initialize_logger('Send_Statistics.txt')
     url = "https://wmf24.ru/api/beveragestatistics"
     headers = {
         'Content-Type': 'application/json'
     }
     response = requests.request("POST", url, headers=headers, data=data)
+    logging.info(f"beveragestatistics: GET request: {data}")
     logging.info(f"beveragestatistics: GET response: {response.text}")
     return True
