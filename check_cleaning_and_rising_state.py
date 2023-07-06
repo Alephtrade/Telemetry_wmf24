e53df598160c37bf9188b25b7b01203cde9ba565
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-07-06 13:48:16 +0300

    update beverages

--- check_cleaning_and_rising_state.py
+++ check_cleaning_and_rising_state.py
@@ -22,11 +22,12 @@ def controller_manager(operator, last_column, duration_column, next_column):
                 db_conn.save_clean_or_rins(duration_column, operator['durationInSeconds']))
             if prev_cleaning_duration != 0:
                 print(db_conn.save_clean_or_rins(last_column, (datetime.now() + timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')))
-    if int(operator["dueInSeconds"]) is not None and int(operator["dueInSeconds"]) != -1:
-        next_datetime = db_conn.get_last_clean_or_rins(next_column)[0]
-        if(next_datetime is None):
-            next_datetime = datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp() + int(operator["dueInSeconds"])))
-            print(db_conn.save_clean_or_rins(next_column, next_datetime))
+    if operator["dueInSeconds"]:
+        if int(operator["dueInSeconds"]) is not None and int(operator["dueInSeconds"]) != -1:
+            next_datetime = db_conn.get_last_clean_or_rins(next_column)[0]
+            if(next_datetime is None):
+                next_datetime = datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp() + int(operator["dueInSeconds"])))
+                print(db_conn.save_clean_or_rins(next_column, next_datetime))
 
 
 print(controller_manager(wm_conn.get_system_cleaning_state(), "last_general_cleaning_datetime", "general_cleaning_duration", "next_general_cleaning_datetime"))
