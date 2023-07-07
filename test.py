a356a0f043b199d94652019a71b3cd87b8b78209
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-07-07 08:30:38 +0300

    data_statistics dev

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import json
 import requests
 import logging
-import telegram.strings as tg_strings
+from api.datastat import methods
 from datetime import timedelta, datetime
 from db.models import WMFSQLDriver
 from core.utils import timedelta_str, get_curr_time, initialize_logger
@@ -14,63 +14,15 @@ DEFAULT_WMF_PARAMS = settings.DEFAULT_WMF_PARAMS
 db_conn = WMFSQLDriver()
 initialize_logger('test.log')
 
-def report_v2():
-
-    global error_text_max_len
-    time_worked = timedelta(minutes=settings.TELEGRAM_REPORT_INTERVAL_MINUTES)
-    date_formed = get_curr_time()
-    date_formed_str = date_formed.strftime('%Y-%m-%d %H:%M:%S')
+def worker():
 
     wm_conn = WMFMachineStatConnector()
     if not wm_conn.ws:
         return False
-    logging.info(f'Successfully connected to machine, part number is {wm_conn.part_number}')
-    data = wm_conn.get_system_cleaning_state()
-    data2 = wm_conn.get_milk_cleaning_state()
-    data3 = wm_conn.get_foamer_rinsing_state()
-    data4 = wm_conn.get_milk_replacement_state()
-    data5 = wm_conn.get_mixer_rinsing_state()
-    data6 = wm_conn.get_milk_mixer_warm_rinsing_state()
-    data7 = wm_conn.get_ffc_filter_replacement_state()
 
+    data_cleaning = methods.get_clean_info
 
-    return {data, data2, data3, data4, data5, data6, data7}
-    data = wm_conn.get_wmf_machine_info()
-    stoppage_time, wmf_error_time = timedelta(), timedelta()
-    stoppage_count, wmf_error_count = 0, 0
-    row_counter = 2
-    unsent_records = db_conn.get_unsent_records()
-    for rec_id, error_code, start_time, end_time, error_text in unsent_records:
-        if end_time:
-            error_text = error_text if error_text else ''
-            duration_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S') - datetime.strptime(start_time,
-                                                                                                 '%Y-%m-%d %H:%M:%S')
-            time_worked -= duration_time
-            if error_code == -1:
-                stoppage_count += 1
-                stoppage_time += duration_time
-            else:
-                wmf_error_count += 1
-                wmf_error_time += duration_time
-
-    data['date_formed'] = date_formed_str
-    data['worked_seconds'] = time_worked.total_seconds()
-    data['time_worked'] = timedelta_str(time_worked)
-    data['wmf_error_seconds'] = wmf_error_time.total_seconds()
-    data['wmf_error_time'] = timedelta_str(wmf_error_time)
-    data['wmf_error_count'] = wmf_error_count
-    data['stoppage_seconds'] = stoppage_time.total_seconds()
-    data['stoppage_time'] = timedelta_str(stoppage_time)
-    data['stoppage_count'] = stoppage_count
-
-    last_record = db_conn.get_last_record()
-    last_bev_count, last_cleaning_datetime = last_record[0], last_record[2]
-    curr_bev_count = wm_conn.get_beverages_count()
-    if curr_bev_count:
-        data['beverages_count'] = curr_bev_count - last_bev_count
-    else:
-        data['beverages_count'] = tg_strings.NO_MACHINE_CONNECTION
-    data['last_cleaning_datetime'] = last_cleaning_datetime
+    return {data_cleaning}
 
     data['code'] = wm_conn.part_number
 
@@ -82,5 +34,5 @@ def report_v2():
     logging.info(f"WMFMachineStatConnector: GET response: {response.text}")
     return response.text
 
-print(report_v2())
+print(worker())
 
