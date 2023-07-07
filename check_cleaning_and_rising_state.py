3a181f07bcaa1c36a48e0dae00c06fd73153a036
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-07-07 13:58:32 +0300

    data_statistics dev

--- check_cleaning_and_rising_state.py
+++ check_cleaning_and_rising_state.py
@@ -60,13 +60,13 @@ def update_activity_info():
             error_text = error_text if error_text else ''
             duration_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S') - datetime.strptime(start_time,
                                                                                                  '%Y-%m-%d %H:%M:%S')
-            time_count_default -= duration_time
+            time_count_default -= int(duration_time)
             if error_code == -1:
                 stoppage_count += 1
-                stoppage_time += duration_time
+                stoppage_time += int(duration_time)
             else:
                 wmf_error_count += 1
-                wmf_error_time += duration_time
+                wmf_error_time += int(duration_time)
     return {
         "unsent_records": unsent_records,
         "summ": summ,
