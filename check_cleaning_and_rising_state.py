4334f5ac1a75eb47de4a80e5e25f4dd4139bc520
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-07-10 08:06:42 +0300

    data_statistics dev

--- check_cleaning_and_rising_state.py
+++ check_cleaning_and_rising_state.py
@@ -26,6 +26,7 @@ def controller_manager(operator, last_column, duration_column, next_column):
                 db_conn.save_clean_or_rins(duration_column, operator['durationInSeconds'])
                 if prev_cleaning_duration != 0:
                     print(db_conn.save_clean_or_rins(last_column, (datetime.now() + timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')))
+        return operator
         if int(operator["dueInSeconds"]) is not None and int(operator["dueInSeconds"]) != -1:
             next_datetime = db_conn.get_last_clean_or_rins(next_column)[0]
             print(operator["dueInSeconds"])
