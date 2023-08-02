18bcadbd88e0026d32ee5cf346ff4cfee0f91b44
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-02 11:24:42 +0300

    error changes

--- error_collector.py
+++ error_collector.py
@@ -39,11 +39,13 @@ def send_errors():
             print("been none")
             try_to_get_part_number = get_part_number_local()
         if wmf_conn.date_error_end is not None:
-            logging.info(f'errors: {wmf_conn.current_errors}')
+            logging.info(f'current_errors: {wmf_conn.current_errors}')
+            logging.info(f'closing_error: {wmf_conn.closing_error}')
             errors = wmf_conn.closing_error
             date_start = wmf_conn.date_error_start
             date_end = wmf_conn.date_error_end
             request = f'{WMF_URL}?code={try_to_get_part_number}&{DEFAULT_WMF_PARAMS}&error_id={errors}&date_start={date_start}&date_end={date_end}status={wmf_conn.get_status()}'
+            wmf_conn.current_errors.remove(wmf_conn.closing_error)
             wmf_conn.date_error_start = None
             wmf_conn.date_error_end = None
             wmf_conn.closing_error = None
@@ -56,6 +58,7 @@ def send_errors():
         elif wmf_conn.get_status() == 0:
             request = f'{WMF_URL}?code={try_to_get_part_number}&{DEFAULT_WMF_PARAMS}&error_id=0&status=0'
         if request:
+            print(request)
             logging.info(f'error_collector send_errors: => {request}')
             response = requests.post(request, timeout=settings.REQUEST_TIMEOUT)
             content = response.content.decode('utf-8')
