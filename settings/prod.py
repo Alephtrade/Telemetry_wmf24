1b5138ea989456f447f17b49b4dbb14529aa321e
it@alephtrade.com <it@alephtrade.com>
2023-08-03 14:58:47 +0300

    fix tg

--- settings/prod.py
+++ settings/prod.py
@@ -7,9 +7,6 @@ WMF_BASE_URL = 'https://wmf24.ru'
 WMF_DATA_URL = "https://wmf24.ru/api/errorhook"
 WS_URL = f'ws://{WS_IP}:{WS_PORT}/'
 DB_PATH = '/root/wmf_1100_1500_5000_router/wmf.db'
-TELEGRAM_BOT_TOKEN = '5797272389:AAFF7214btV4YpA0GgCQDmuRfi-zff2EtIM'
-TELEGRAM_CHAT_ID = ''
-TELEGRAM_DEBUGGING_ID = '-1001516057504'
 ERROR_COLLECTOR_INTERVAL_SECONDS = 5
 CHECK_MACHINE_STATUS_INTERNAL = 1 * 60
 REQUEST_TIMEOUT = 5
@@ -21,23 +18,3 @@ TELEGRAM_REPORT_RECORDS_TO_KEEP = 30
 CLEAN_DB_DAYS = 30
 REQUEST_RETRIES = 5
 
-
-def get_chat_id():
-    if TELEGRAM_CHAT_ID == '' :
-        try:
-            with open('/root/wmf_1100_1500_5000_router/part_number.txt') as f:
-                part_number = f.read()
-        except Exception:
-            return ''
-        #print(part_number)
-        url = f'{WMF_BASE_URL}/api/get-coffee-machine-info/{part_number}'
-        #print(url)
-        r = requests.get(url)
-        #print(r)
-        data = r.json()
-        #print(data)
-        s = data["telegram_chat_id"]
-        return TELEGRAM_CHAT_ID
-
-
-print(get_chat_id())
