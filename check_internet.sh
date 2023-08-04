22315c5b2b9eaaa6d944926ee789eb8c8429ed54
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-04 13:30:13 +0300

    f

--- check_internet.sh
+++ check_internet.sh
@@ -7,10 +7,5 @@ if ping -c $COUNT $IP > /dev/null 2>&1; then
   echo "$TIME : Ping to $IP was successful."
 else
   echo "$TIME : Ping to $IP failed."
-  /etc/init.d/network restart
-  if ping -c $COUNT $IP > /dev/null 2>&1; then
-    echo "$TIME : Ping after network restarted to $IP was successful."
-  else
-    reboot
-  fi
+  reboot
 fi
