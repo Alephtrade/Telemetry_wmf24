132bb8573e08b19df7609a96faf23181258b037a
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-23 10:41:51 +0300

    error_fix_test

--- test.py
+++ test.py
@@ -1,16 +1,8 @@
-import json
-import logging
-import websocket
-import ast
-from datetime import datetime, timedelta
 import sys
-import requests
 
 sys.path.append("../../")
 from db.models import WMFSQLDriver
-from core.utils import initialize_logger, get_beverages_send_time, get_part_number_local
 from settings import prod as settings
-from wmf.models import WMFMachineStatConnector
 
 WMF_URL = settings.WMF_DATA_URL
 WS_URL = settings.WS_URL
