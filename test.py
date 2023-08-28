ddef550bdd9e106d56628674565e4f0a0a7dba60
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-28 11:07:35 +0300

    error_fix_test

--- test.py
+++ test.py
@@ -13,8 +13,11 @@ db_conn = WMFSQLDriver()
 def worker():
     last_error_id = db_conn.get_error_last_record()
     print(last_error_id)
-    if (last_error_id[0] != "62" and last_error_id[0] != "-1" and last_error_id[1] is not None):
-        return True
+    if last_error_id != [] and last_error_id is not None:
+        if (last_error_id[0] != "62" and last_error_id[0] != "-1" and last_error_id[1] is not None):
+            return True
+    else:
+        print("not None")
 
 
 print(worker())
