1d36f8db056c79ff2508d70a02a934158370809a
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-08-28 12:10:55 +0300

    BEv fix

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import sys
+import websocket
+import logging
+import requests
+import json
+import ast
+import socket
+from datetime import timedelta, datetime
+from api.beverages import methods
+
 
 sys.path.append("../../")
 from db.models import WMFSQLDriver
@@ -11,13 +20,30 @@ db_conn = WMFSQLDriver()
 
 
 def worker():
-    last_error_id = db_conn.get_error_last_record()
-    print(last_error_id)
-    if last_error_id != [] and last_error_id is not None:
-        if (last_error_id[0] != "62" and last_error_id[0] != "-1" and last_error_id[1] is not None):
-            return True
+    k = []
+    time_to_send = None
+    receive_data = db_conn.get_not_sended_beverages_log()
+    if(receive_data == []):
+        print(f'NO DATA')
     else:
-        print("not None")
+        for item in receive_data:
+            print(f'loop')
+            time_to_send = item[2]
+            k.append({"device_code": item[0]})
+            k.append({"summ": item[1]})
+            k.append({"time_to_send": item[2]})
+            k.append({"time_fact_send": item[3]})
+            k.append({"date_formed": item[4]})
+            data_info = ast.literal_eval(str(item[5]))
+            record_id = item[6]
+            for item_info in data_info:
+                k.append(item_info)
+            next_time = datetime.strptime(time_to_send, '%Y-%m-%d %H:%M:%S')
+            if datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp())) > next_time:
+                print(methods.Send_Statistics(json.dumps(k), record_id))
+                print(f'Send_Statistics db id - {record_id}')
+            else:
+                print(f'wrong time to_sent - {next_time}')
 
 
 print(worker())
