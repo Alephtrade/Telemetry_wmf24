de5aa0b425c82bc590f9fa02b5bf4b0b080093c1
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-09-06 08:54:13 +0300

    part number fix

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from api.beverages import methods
 sys.path.append("../../")
 from db.models import WMFSQLDriver
 from settings import prod as settings
-from wmf.models import WMFMachineStatConnector
+from wmf.models import WMFMachineStatConnector, WMFMachineErrorConnector
 from core.utils import initialize_logger, get_beverages_send_time
 
 
@@ -20,10 +20,13 @@ WMF_URL = settings.WMF_DATA_URL
 WS_URL = settings.WS_URL
 DEFAULT_WMF_PARAMS = settings.DEFAULT_WMF_PARAMS
 db_conn = WMFSQLDriver()
-
+wmf_conn = WMFMachineErrorConnector()
+wmf2_conn = WMFMachineStatConnector()
 
 def worker():
-    last_error_id = db_conn.get_error_last_record()
-    return last_error_id
+    last_error_id = wmf_conn.get_part_number()
+    last_error_id2 = wmf2_conn.get_part_number()
+
+    return [last_error_id, last_error_id2]
 
 print(worker())
