f2b1b2c34b06684c695c5bf04243a0a44dbe3602
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-09-06 14:07:47 +0300

    data_stat_fix

--- activity_beverages_service_creator.py
+++ activity_beverages_service_creator.py
@@ -19,7 +19,8 @@ def get_main_clean_stat():
     initialize_logger('controller_cleaning_statistic_creator.py.log')
     time_now = datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp() // (60 * 60) * 60 * 60))
     now = datetime.fromtimestamp(int((datetime.now() + timedelta(hours=3)).timestamp()))
-    date_to_send = get_beverages_send_time(now)
+    get_last_data_statistics = db_conn.get_last_data_statistics()
+    date_to_send = get_beverages_send_time(get_last_data_statistics)
     db_conn.create_data_statistics(time_now, date_to_send)
     stoppage_time, wmf_error_time, time_count_default = timedelta(), timedelta(), timedelta(seconds=3600)
     stoppage_count, wmf_error_count = 0, 0
