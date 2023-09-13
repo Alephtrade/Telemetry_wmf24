44e7b9ac859d91cb7316795284b3b131cba2efcc
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-09-13 15:15:18 +0300

    fix

--- test.py
+++ test.py
@@ -26,7 +26,22 @@ wmf2_conn = WMFMachineStatConnector()
 def worker():
     try_to_get_part_number = get_part_number_local()
     if try_to_get_part_number is None:
-        try_to_get_part_number = wmf_conn.get_part_number()
+        try_to_get_part_number = get_part_number()
     print(try_to_get_part_number)
+
+def get_part_number():
+    try:
+         ws = websocket.create_connection(WS_URL)
+         request = json.dumps({'function': 'getMachineInfo'})
+         logging.info(f"COFFEE_MACHINE: Sending {request}")
+         ws.send(request)
+         received_data = ws.recv()
+         print(received_data)
+         ws.close()
+         return WMFMachineStatConnector.normalize_json(received_data).get('PartNumber')
+     except Exception as ex:
+         logging.info(f"COFFEE_MACHINE: Get machine info error, HOST {WS_URL} - {ex}")
+         print(ex)
+         return None
 
 print(worker())
