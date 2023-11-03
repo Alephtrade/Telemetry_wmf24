ce963770a247d64d042f08b8c30273a0f0318332
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-11-03 14:13:13 +0300

    init

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from controllers.test import worker
 from flask import jsonify
-
+from controllers.wmf.ssh_send_com import send_wmf_request
+import sys
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return jsonify(worker()), 200
 
+@app.route('/console')
+def terra():  # put application's code here
+    print('---')
+    c = sys.argv
+    return jsonify(send_wmf_request(c[1]), 200)
 
 if __name__ == '__main__':
     app.run()
