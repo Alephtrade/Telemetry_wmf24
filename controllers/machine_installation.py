d8791d137594516d5ae931d57bc5b4b58b2f6280
Dmitry <dimonstalkers@mail.ru>
2023-11-28 12:38:11 +0300

    fix

--- controllers/machine_installation.py
+++ controllers/machine_installation.py
@@ -2,16 +2,7 @@ import nmap
 
 def test():
     nm = nmap.PortScanner()
-    hosts = nm.scan(hosts='192.168.1.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
-    return hosts
-    for host in nm.all_hosts():
-        print('Host : %s (%s)' % (host, nm[host].hostname()))
-        print('State : %s' % nm[host].state())
-        for proto in nm[host].all_protocols():
-            print('----------')
-            print('Protocol : %s' % proto)
+    hosts = nm.scan(hosts='10.8.0.0/24', arguments='-sn')
 
-            lport = nm[host][proto].keys()
-            lport.sort()
-            for port in lport:
-                print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    for host in hosts["scan"]:
+        return host
