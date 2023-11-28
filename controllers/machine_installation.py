58bee60bd31a925a8e6b9ac2a989a965cbec12b4
Dmitry <dimonstalkers@mail.ru>
2023-11-28 12:42:06 +0300

    fix

--- controllers/machine_installation.py
+++ controllers/machine_installation.py
@@ -1,8 +1,11 @@
 import nmap
 
+
 def test():
     nm = nmap.PortScanner()
     hosts = nm.scan(hosts='10.8.0.0/24', arguments='-sn')
-
+    ips = []
     for host in hosts["scan"]:
-        return host
+        ips.append(host)
+
+    return ips
