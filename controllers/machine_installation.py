dc2df8915d4cb8622beb2c92c75d94910123cd3e
Dmitry <dimonstalkers@mail.ru>
2023-11-28 13:17:42 +0300

    fix

--- controllers/machine_installation.py
+++ controllers/machine_installation.py
@@ -1,4 +1,8 @@
 import nmap
+import requests
+import websocket
+import json
+import logging
 
 
 def test():
@@ -6,6 +10,20 @@ def test():
     hosts = nm.scan(hosts='10.8.0.0/24', arguments='-sn')
     ips = []
     for host in hosts["scan"]:
-        ips.append(host)
-
+        ips.append(require(host))
     return ips
+
+def require(ip):
+    WS_URL = f'ws://{ip}:25000/'
+    try:
+        ws = websocket.create_connection(WS_URL, timeout=5)
+    except Exception:
+        ws = None
+    request = json.dumps({'function': 'getMachineInfo'})
+    print(request)
+    print("------------------------------")
+    logging.info(f"WMFMachineStatConnector: Sending {request}")
+    ws.send(request)
+    received_data = ws.recv()
+    logging.info(f"WMFMachineStatConnector: Received {received_data}")
+    return received_data
