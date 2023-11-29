2ce1ef4157974b9581cf07ecab5394b15f474f42
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-11-29 11:40:14 +0300

    установка

--- controllers/add_cron_jobs.sh
+++ controllers/add_cron_jobs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-croncmd="sh /root/wmf_1100_1500_5000_router/start_error_collector.sh >> start_error_collector.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/start_error_collector.sh >> start_error_collector.txt 2>&1"
 cronjob="* * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="sh /root/wmf_1100_1500_5000_router/git_pull_autorestart.sh >> git_pull_autorestart.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/git_pull_autorestart.sh >> git_pull_autorestart.txt 2>&1"
 cronjob="0 0 * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="sh /root/wmf_1100_1500_5000_router/check_internet.sh >> check_internet.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/check_internet.sh >> check_internet.txt 2>&1"
 cronjob="*/30 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="sh /root/wmf_1100_1500_5000_router/run_check_cleaning_and_rising.sh >> run_check_cleaning_and_rising.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/run_check_cleaning_and_rising.sh >> run_check_cleaning_and_rising.txt 2>&1"
 cronjob="*/5 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="sh /root/wmf_1100_1500_5000_router/run_activity_beverages_sender.sh >> run_activity_beverages_sender.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/run_activity_beverages_sender.sh >> run_activity_beverages_sender.txt 2>&1"
 cronjob="* * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
-croncmd="sh /root/wmf_1100_1500_5000_router/run_activity_beverages_service_creator.sh >> run_activity_beverages_service_creator.txt 2>&1"
+croncmd="sh /var/www/Telemetry_wmf24/run_activity_beverages_service_creator.sh >> run_activity_beverages_service_creator.txt 2>&1"
 cronjob="0 * * * * $croncmd"
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
