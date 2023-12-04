8aaba70c4a679ce6a322665b1c59dfbe9330896c
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-12-04 14:00:09 +0300

    установка

--- controllers/error_collector.py
+++ controllers/error_collector.py
@@ -15,7 +15,7 @@ from controllers.settings import prod as settings
 from controllers.db.models import WMFSQLDriver
 
 threads = {}
-aleph_id = Timeloop()
+tl = Timeloop()
 db_conn = WMFSQLDriver()
 
 
@@ -30,13 +30,13 @@ def worker(aleph_id, ip):
     def on_exit():
         try:
             wmf_conn.close()
-            aleph_id.stop()
+            tl.stop()
         except Exception as ex:
             logging.error(f'error_collector on_exit: ERROR={ex}')
             logging.error(print_exception())
 
 
-    @aleph_id.job(interval=timedelta(seconds=settings.ERROR_COLLECTOR_INTERVAL_SECONDS))
+    @tl.job(interval=timedelta(seconds=settings.ERROR_COLLECTOR_INTERVAL_SECONDS))
     def send_errors():
         try:
             logging.info("error_collector send_errors: CALL")
@@ -68,7 +68,7 @@ def worker(aleph_id, ip):
         except Exception as ex:
             logging.error(f'error_collector send_errors: ERROR={ex}, stacktrace: {print_exception()}')
 
-    aleph_id.start()
+    tl.start()
     logging.info('error_collector.py started and running...')
     return "error_collector.py started and running..."
     atexit.register(on_exit)
