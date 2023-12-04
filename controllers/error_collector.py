0714dcb3c8f02f3f49903ddb671837ff5d1dbc41
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-12-04 15:24:12 +0300

    установка

--- controllers/error_collector.py
+++ controllers/error_collector.py
@@ -6,6 +6,7 @@ import threading
 from datetime import timedelta
 from timeloop import Timeloop
 import sys
+
 sys.path.append('./')
 sys.path.append('/var/www/Telemetry_wmf24/')
 from controllers.core.utils import initialize_logger, print_exception, get_env_mode, get_part_number_local
@@ -24,52 +25,54 @@ def worker(tl_ident, aleph_id, ip):
     print(ip)
     initialize_logger('error_collector.log')
     tl_ident.start()
-
-    def on_exit(tl_ident, wmf_conn):
-        try:
-            wmf_conn.close()
-            tl_ident.stop()
-        except Exception as ex:
-            logging.error(f'error_collector on_exit: ERROR={ex}')
-            logging.error(print_exception())
+    return tl_ident.is_alive()
 
 
-    @tl_ident.job(interval=timedelta(seconds=settings.ERROR_COLLECTOR_INTERVAL_SECONDS))
-    def send_errors():
-        try:
-            logging.info("error_collector send_errors: CALL")
-            errors, request = '', ''
-            unset_errors = db_conn.get_unsent_records()
-            if unset_errors:
-                for record in unset_errors:
-                    #request = f'{WMF_URL}?code={try_to_get_part_number}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[5]}&status={wmf_conn.get_status()}'
-                    #response = requests.post(request)
-                    #content = response.content.decode('utf-8')
-                    if record[3] is not None:
-                        db_conn.set_report_sent(record[0])
-                    else:
-                        db_conn.set_report_pre_sent(record[0])
-                    #logging.info(f'error_collector send_errors: <= {response} {content}')
-            else:
-                unset_errors = db_conn.get_unsent_records_with_end_time()
+def on_exit(tl_ident, wmf_conn):
+    try:
+        wmf_conn.close()
+        tl_ident.stop()
+    except Exception as ex:
+        logging.error(f'error_collector on_exit: ERROR={ex}')
+        logging.error(print_exception())
+
+    if not tl_ident.is_alive():
+        @tl_ident.job(interval=timedelta(seconds=settings.ERROR_COLLECTOR_INTERVAL_SECONDS))
+        def send_errors():
+            try:
+                logging.info("error_collector send_errors: CALL")
+                errors, request = '', ''
+                unset_errors = db_conn.get_unsent_records()
                 if unset_errors:
                     for record in unset_errors:
-                        #request = f'{WMF_URL}?code={try_to_get_part_number}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[5]}&status={wmf_conn.get_status()}'
-                        #response = requests.post(request)
-                        #content = response.content.decode('utf-8')
-                        db_conn.set_report_sent(record[0])
-                        #logging.info(f'error_collector send_errors: <= {response} {content}')
+                        # request = f'{WMF_URL}?code={try_to_get_part_number}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[5]}&status={wmf_conn.get_status()}'
+                        # response = requests.post(request)
+                        # content = response.content.decode('utf-8')
+                        if record[3] is not None:
+                            db_conn.set_report_sent(record[0])
+                        else:
+                            db_conn.set_report_pre_sent(record[0])
+                        # logging.info(f'error_collector send_errors: <= {response} {content}')
                 else:
-                    #request = f'{WMF_URL}?code={try_to_get_part_number}&error_id=0&status={wmf_conn.get_status()}'
-                    #response = requests.post(request)
-                    logging.info(f'error_collector send_errors: nothing to send')
-        except Exception as ex:
-            logging.error(f'error_collector send_errors: ERROR={ex}, stacktrace: {print_exception()}')
+                    unset_errors = db_conn.get_unsent_records_with_end_time()
+                    if unset_errors:
+                        for record in unset_errors:
+                            # request = f'{WMF_URL}?code={try_to_get_part_number}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[5]}&status={wmf_conn.get_status()}'
+                            # response = requests.post(request)
+                            # content = response.content.decode('utf-8')
+                            db_conn.set_report_sent(record[0])
+                            # logging.info(f'error_collector send_errors: <= {response} {content}')
+                    else:
+                        # request = f'{WMF_URL}?code={try_to_get_part_number}&error_id=0&status={wmf_conn.get_status()}'
+                        # response = requests.post(request)
+                        logging.info(f'error_collector send_errors: nothing to send')
+            except Exception as ex:
+                logging.error(f'error_collector send_errors: ERROR={ex}, stacktrace: {print_exception()}')
 
-    #tl.start()
-    logging.info('error_collector.py started and running...')
-    atexit.register(on_exit)
-    return tl_ident.is_alive()
+        # tl.start()
+        logging.info('error_collector.py started and running...')
+        atexit.register(on_exit)
+        return tl_ident.is_alive()
 
 
 db_conn = WMFSQLDriver()
@@ -80,4 +83,3 @@ for device in devices:
     tl_ident = threading.Thread(target=wmf_conn.run_websocket)
     result = worker(tl_ident, device[1], device[2])
     print(result)
-
