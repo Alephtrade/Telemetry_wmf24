b3b7c8c21d3ca4cbc50e1de48b2b7341cc9627f6
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-12-11 09:05:57 +0300

    logger up cleaning php response

--- controllers/core/utils.py
+++ controllers/core/utils.py
@@ -80,6 +80,7 @@ def get_beverages_send_time(last_send_time):
         minutes_to_go = 0
     else:
         minutes_to_go = minutes_to_go[0]
+    print(minutes_to_go[0])
     a = int(next_time.timestamp() // (60 * 60) * 60 * 60) + int(INTERVAL) * 60 * 60 + minutes_to_go * 60
     if a < int(datetime.now().timestamp()):
         a = int(int(datetime.now().timestamp()) // (60 * 60) * 60 * 60) + int(INTERVAL) * 60 * 60 + minutes_to_go * 60
