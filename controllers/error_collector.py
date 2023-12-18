091768c16cfca05075d119912c82d5648bd30214
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2023-12-18 13:52:07 +0300

    current_errors

--- controllers/error_collector.py
+++ controllers/error_collector.py
@@ -36,44 +36,45 @@ def worker(ip):
             logging.error(print_exception())
 
     @tl_ident.job(interval=timedelta(seconds=settings.ERROR_COLLECTOR_INTERVAL_SECONDS))
-    def send_errors():
-        try:
-            logging.info("error_collector send_errors: CALL")
-            errors, request = '', ''
-            unset_errors = db_conn.get_unsent_records(device[1])
-            print(unset_errors)
-            if unset_errors:
-                for record in unset_errors:
-                    print(record)
-                    request = f'{WMF_URL}?device={device[1]}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[4]}&status={wmf_conn.get_status()}'
-                    print("errorrrrrrrrrrrrrrrrrrrrr")
-                    print(request)
-                    response = requests.post(request)
-                    content = response.content.decode('utf-8')
-                    if record[3] is not None:
-                        db_conn.set_report_sent(record[0])
-                    else:
-                        db_conn.set_report_pre_sent(record[0])
-                    logging.info(f'error_collector send_errors: <= {response} {content}')
-            else:
-                unset_errors = db_conn.get_unsent_records_with_end_time(device[1])
+    def send_errors(devices):
+        for device in devices:
+            try:
+                logging.info("error_collector send_errors: CALL")
+                errors, request = '', ''
+                unset_errors = db_conn.get_unsent_records(device[1])
+                print(unset_errors)
                 if unset_errors:
                     for record in unset_errors:
+                        print(record)
                         request = f'{WMF_URL}?device={device[1]}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[4]}&status={wmf_conn.get_status()}'
-                        print("72")
+                        print("errorrrrrrrrrrrrrrrrrrrrr")
                         print(request)
                         response = requests.post(request)
                         content = response.content.decode('utf-8')
-                        db_conn.set_report_sent(record[0])
+                        if record[3] is not None:
+                            db_conn.set_report_sent(record[0])
+                        else:
+                            db_conn.set_report_pre_sent(record[0])
                         logging.info(f'error_collector send_errors: <= {response} {content}')
                 else:
-                    request = f'{WMF_URL}?device={device[1]}&error_id=0&status={wmf_conn.get_status()}'
-                    print("79")
-                    print(request)
-                    response = requests.post(request)
-                    logging.info(f'error_collector send_errors: nothing to send')
-        except Exception as ex:
-            logging.error(f'error_collector send_errors: ERROR={ex}, stacktrace: {print_exception()}')
+                    unset_errors = db_conn.get_unsent_records_with_end_time(device[1])
+                    if unset_errors:
+                        for record in unset_errors:
+                            request = f'{WMF_URL}?device={device[1]}&error_id={record[1]}&date_start={record[2]}&date_end={record[3]}&duration={record[4]}&status={wmf_conn.get_status()}'
+                            print("72")
+                            print(request)
+                            response = requests.post(request)
+                            content = response.content.decode('utf-8')
+                            db_conn.set_report_sent(record[0])
+                            logging.info(f'error_collector send_errors: <= {response} {content}')
+                    else:
+                        request = f'{WMF_URL}?device={device[1]}&error_id=0&status={wmf_conn.get_status()}'
+                        print("79")
+                        print(request)
+                        response = requests.post(request)
+                        logging.info(f'error_collector send_errors: nothing to send')
+            except Exception as ex:
+                logging.error(f'error_collector send_errors: ERROR={ex}, stacktrace: {print_exception()}')
 
     tl_ident.start()
     logging.info('error_collector.py started and running...')
