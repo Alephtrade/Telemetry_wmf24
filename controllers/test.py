2794c499839d53849379440aab48797a585014e3
Dmitry <dimonstalkers@mail.ru>
2023-12-19 15:24:09 +0300

    pours

--- controllers/test.py
+++ controllers/test.py
@@ -4,7 +4,7 @@ import logging
 import requests
 import json
 import time
-
+from collections import deque
 import ast
 import socket
 import sys
@@ -18,5 +18,19 @@ db_conn = WMFSQLDriver()
 devices = db_conn.get_devices()
 for device in devices:
     WS_URL = f'ws://{devices[2]}:{settings.WS_PORT}/'
-    ws = websocket.create_connection(WS_URL, timeout=settings.WEBSOCKET_CONNECT_TIMEOUT)
-    print(ws.send(json.dumps({"function": "getRecipeComposition", "RecipeNumber": 27})).recv())
+    ws = websocket.create_connection(WS_URL, timeout=5)
+    request = json.dumps({"function": "getRecipeComposition", "RecipeNumber": 27})
+    print()
+    print(request)
+    print("------------------------------")
+    logging.info(f"WMFMachineStatConnector: Sending {request}")
+    ws.send(request)
+    received_data = ws.recv()
+    logging.info(f"WMFMachineStatConnector: Received {received_data}")
+    received_data2 = deque(json.loads(received_data))
+    formatted = {}
+    for var in list(received_data2):
+        for i in var:
+            formatted[i] = var[i]
+    formatted["ip"] = ip
+    return formatted
