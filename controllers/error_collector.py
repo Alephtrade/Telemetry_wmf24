9a5ffb8f71981d4262c452b0fcbc4298db4080c1
Dmitry <dimonstalkers@mail.ru>
2023-12-21 01:04:56 +0300

    pours

--- controllers/error_collector.py
+++ controllers/error_collector.py
@@ -85,12 +85,12 @@ def worker(ip):
 for device in devices:
     try:
         ws = websocket.create_connection(f'ws://{device[2]}:25000/', timeout=5)
-        print("DONE")
+        print({device[1] + "DONE"})
     except Exception:
-        print("FALSE")
-    wmf_conn = WMFMachineErrorConnector(device[1], device[2])
-    threading.Thread(target=wmf_conn.run_websocket, name=device[1]).start()
-    worker(device[2])
+        print({device[1] + "FALSE"})
+    #wmf_conn = WMFMachineErrorConnector(device[1], device[2])
+    #threading.Thread(target=wmf_conn.run_websocket, name=device[1]).start()
+    #worker(device[2])
     print(threading.active_count())
     print(threading.enumerate())
 
