38d49ed4745ab4cbc999040987b055bcd1f3f7f1
Dmitry <dimonstalkers@mail.ru>
2023-12-21 15:26:55 +0300

    ips

--- controllers/machine_installation.py
+++ controllers/machine_installation.py
@@ -27,7 +27,7 @@ def test():
             data_for_request["model"] = machine_response["ProductName"]
             data_for_request["ip"] = machine_response["ip"]
             print(machine_response)
-            if machine_response and machine_response["ip"] is not None:
+            if machine_response:
                 url = "https://backend.wmf24.ru/api/machine_check"
 
                 headers = {
@@ -62,7 +62,7 @@ def require_info(ip):
     try:
         ws = websocket.create_connection(WS_URL, timeout=5)
     except Exception:
-        ws = None
+        return False
     request = json.dumps({'function': 'getMachineInfo'})
     print()
     print(request)
