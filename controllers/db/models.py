fe1200fb87162d1358dab140195c53b2c6ec5cee
Dmitry <dimonstalkers@mail.ru>
2023-12-22 13:20:16 +0300

    drinks

--- controllers/db/models.py
+++ controllers/db/models.py
@@ -15,10 +15,10 @@ class WMFSQLDriver:
     def close(self):
         self.connection.close()
 
-    def initRecipe(self, aleph_id, recipe_id, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight):
+    def initRecipe(self, aleph_id, recipe_id, recipe_alias, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight):
         cur = self.connection.cursor()
-        stmt = 'INSERT INTO recipes (aleph_id, recipe_id, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-        cur.execute(stmt, (aleph_id, recipe_id, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight,))
+        stmt = 'INSERT INTO recipes (aleph_id, recipe_id, recipe_alias, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        cur.execute(stmt, (aleph_id, recipe_id, recipe_alias, coffee_count, coffee_weight, water_count, water_weight, milk_count, milk_weight, powder_count, powder_weight, foam_count, foam_weight,))
         self.connection.commit()
         cur.close()
     def updateRecipe(self):
