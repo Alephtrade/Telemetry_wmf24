a686ba2775634ba537710084134372e0780d4117
Dmitry <dimonstalkers@mail.ru>
2023-12-29 00:23:15 +0300

    beverages

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -117,7 +117,8 @@ def Take_Create_Beverage_Statistics(last_send, device):
                                     if middle_recipe is None or middle_recipe == []:
                                         from controllers.api.beverages.Drinks import updateDrinks
                                         updateDrinks(device[1], device[2])
-                                    print(middle_recipe)
+                                    if middle_recipe is None or len(middle_recipe) < 1:
+                                        middle_recipe = db_conn.getRecipe(device[1], recipe_number)
                                     if middle_recipe is not None and len(middle_recipe) > 1:
                                         db_conn.initPours(device[1], recipe_number, middle_recipe[0][3], recipe_size, middle_recipe[0][4], middle_recipe[0][5], middle_recipe[0][6], middle_recipe[0][7], middle_recipe[0][8])
                                         print("Должен был создаться мидл")
