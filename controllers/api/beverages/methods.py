adf2fc300f71734382f496319ad213bbc37e3d3c
Dmitry <dimonstalkers@mail.ru>
2023-12-28 23:12:10 +0300

    beverages

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -111,7 +111,7 @@ def Take_Create_Beverage_Statistics(last_send, device):
                                 print(count_of_real_pours)
                                 print("В базе найдено")
                                 print(len(pours_detected_in_base))
-                                if len(pours_detected_in_base) == 0 or pours_detected_in_base is None:
+                                if len(pours_detected_in_base) < 1 or pours_detected_in_base is None or pours_detected_in_base == 0:
                                     print("Поиск мидла")
                                     middle_recipe = db_conn.get_pours_with_recipeId_and_cup_size(device[1], recipe_number, "M", time_now, prev_hour)
                                     if middle_recipe is None or middle_recipe == []:
