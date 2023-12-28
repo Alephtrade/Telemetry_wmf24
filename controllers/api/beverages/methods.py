0c06d300676aceaef90d6cd732b89e8c64e6bd60
Dmitry <dimonstalkers@mail.ru>
2023-12-29 01:48:13 +0300

    beverages

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -103,7 +103,7 @@ def Take_Create_Beverage_Statistics(last_send, device):
                             print("DIFFERENCE")
                             time_now = date_formed
                             prev_hour = time_now - timedelta(hours=24)
-                            time_to_form = prev_hour + timedelta(minutes=30)
+                            time_to_form = date_formed - timedelta(minutes=30)
                             count_of_real_pours = int(elem) - int(last_info[k])
                             print("REQUEST")
                             print(device[1])
@@ -111,6 +111,7 @@ def Take_Create_Beverage_Statistics(last_send, device):
                             print(recipe_size)
                             print(time_now)
                             print(prev_hour)
+                            print(time_to_form)
                             pours_detected_in_base = db_conn.get_pours_with_recipeId_and_cup_size(device[1], recipe_number, recipe_size, time_now, prev_hour)
                             print("Должно быть")
                             print(count_of_real_pours)
