8d5bb78b6a9480ed29c59b13358de9da4049965a
Dmitry <dimonstalkers@mail.ru>
2023-12-29 00:46:40 +0300

    beverages

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -131,7 +131,9 @@ def Take_Create_Beverage_Statistics(last_send, device):
                                             middle_recipe[0][6] = recipe_callback[10]
                                             middle_recipe[0][7] = recipe_callback[12]
                                             middle_recipe[0][8] = recipe_callback[14]
-                                            db_conn.initPours(device[1], recipe_number, middle_recipe[0][3], recipe_size, middle_recipe[0][4], middle_recipe[0][5], middle_recipe[0][6], middle_recipe[0][7], middle_recipe[0][8])
+                                            while count_of_real_pours > pours_detected_in_base :
+                                                db_conn.initPours(device[1], recipe_number, middle_recipe[0][3], recipe_size, middle_recipe[0][4], middle_recipe[0][5], middle_recipe[0][6], middle_recipe[0][7], middle_recipe[0][8])
+                                                pours_detected_in_base += 1
                                             print("Должен был создаться мидл")
                                             created["aleph_id"] = device[1]
                                             created["recipe_id"] = recipe_number
