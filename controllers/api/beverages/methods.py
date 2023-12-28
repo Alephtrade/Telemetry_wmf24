0b420354c70c4a661c8bc6956f589770f558b326
Dmitry <dimonstalkers@mail.ru>
2023-12-29 02:05:01 +0300

    beverages

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -129,6 +129,7 @@ def Take_Create_Beverage_Statistics(last_send, device):
                                         updateDrinks(device[1], device[2])
                                     middle_recipe = db_conn.get_pours_with_recipeId_and_cup_size(device[1], recipe_number, "M", time_now, prev_hour)
                                     print("MIDDLE")
+                                    print(middle_recipe[0][12])
                                     print(middle_recipe)
                                     if middle_recipe[0][12] == 0:
                                         recipe_callback = db_conn.getRecipe(device[1], recipe_number)
