d956e0e22de354a5e9863e86a6de7efb3c91be93
Dmitry <dimonstalkers@mail.ru>
2023-12-29 03:59:46 +0300

    pours sender

--- controllers/activity_beverages_sender.py
+++ controllers/activity_beverages_sender.py
@@ -48,7 +48,7 @@ def beverages_send_worker(aleph_id, ip):
     sorter = {}
     not_sort_pours = db_conn.get_all_pours_not_sended(device[1])
     for key in not_sort_pours:
-            print(key)
+            print(key[3])
 
     return True
 
