75073917dd7769bc6e94201d0da53ab3c425543d
Dmitry <dimonstalkers@mail.ru>
2024-01-08 05:23:30 +0300

    logs disabled

--- controllers/activity_beverages_service_creator.py
+++ controllers/activity_beverages_service_creator.py
@@ -184,5 +184,5 @@ devices = db_conn.get_devices()
 for device in devices:
     print(device[2])
     print(are_need_to_create(device))
-    print(get_service_statistics(device))
-    print(get_main_clean_stat(device))
+    #print(get_service_statistics(device))
+    #print(get_main_clean_stat(device))
