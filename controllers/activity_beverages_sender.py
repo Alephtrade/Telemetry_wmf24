cd11db5352cf78a97e7758fdbcf56ea99f91a9c3
Dmitry <dimonstalkers@mail.ru>
2024-01-08 06:57:02 +0300

    logs disabled

--- controllers/activity_beverages_sender.py
+++ controllers/activity_beverages_sender.py
@@ -36,8 +36,7 @@ def beverages_send_worker(aleph_id, ip):
             record_id = item[6]
             for item_info in data_info:
                 k.append(item_info)
-                print(item_info.keys())
-                if item_info.keys() != 'TotalCountRcp':
+                if item_info.keys()[0] != 'TotalCountRcp':
                     print(item_info)
             next_time = datetime.strptime(time_to_send, '%Y-%m-%d %H:%M:%S')
             if datetime.fromtimestamp(int(datetime.now().timestamp())) > next_time:
