f1184be6607641c4f2db84e47c90d79696b1170d
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2024-01-10 08:09:59 +0300

    recipe request

--- controllers/activity_beverages_service_creator.py
+++ controllers/activity_beverages_service_creator.py
@@ -31,6 +31,8 @@ def get_main_clean_stat(device):
     #print(date_to_send)
     db_conn.create_data_statistics(device[1], time_now, date_to_send)
     unsent_records = db_conn.get_error_records(prev_hour, time_now, device[1])
+    print({"Ошибки": unsent_records})
+    print({prev_hour, time_now, device[1]})
     unsent_disconnect_records = db_conn.get_all_error_records_by_code(device[1], prev_hour, time_now, "-1")
     print({"Ошибки -1": unsent_disconnect_records})
     date_end_prev_error = prev_hour
@@ -41,7 +43,7 @@ def get_main_clean_stat(device):
     wmf_error_count = 0
     disconnect_count = 0
     for rec_id, error_code, start_time, end_time in unsent_records:
-        print({"Ошибки": unsent_records})
+        print({"Ошибки3": unsent_records})
         if type(start_time) is not datetime and start_time is not None:
             start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
         if type(end_time) is not datetime and end_time is not None:
