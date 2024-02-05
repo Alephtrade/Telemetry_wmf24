0d1790b106e695544134be5ccc1a20cfeca56d66
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2024-02-05 08:34:02 +0300

    test socket

--- controllers/test.py
+++ controllers/test.py
@@ -20,7 +20,8 @@ for device in devices:
     WS_URL = f'ws://{device[2]}:25000/'
     wmf_conn = WMFMachineErrorConnector(device[1], device[2])
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        if(device[2] is not None):
+        print(device[2])
+        if device[2] is not None:
             s.connect((device[2], 25000))
             s.send('{"function": "startPushDispensingFinished"}'.encode())
             s.send('{"function": "getMachineInfo"}'.encode())
