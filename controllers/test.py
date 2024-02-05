ef2d64d0b7ac702214262a2b4c803352b6b8b111
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2024-02-05 08:25:25 +0300

    test socket

--- controllers/test.py
+++ controllers/test.py
@@ -10,46 +10,21 @@ from controllers.db.models import WMFSQLDriver
 from controllers.settings import prod as settings
 from controllers.core.utils import timedelta_int, get_beverages_send_time, initialize_logger
 from controllers.api.beverages import methods
-from controllers.wmf.models import WMFMachineStatConnector
+from controllers.wmf.models import WMFMachineStatConnector, WMFMachineErrorConnector
+
 db_conn = WMFSQLDriver()
 
 devices = db_conn.get_devices()
 for device in devices:
     db_driver = WMFSQLDriver()
     WS_URL = f'ws://{device[2]}:25000/'
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((device[2], 25000))
-    server.listen(15)
-    print(f'Server{device[2]} 25000 start.')
+    wmf_conn = WMFMachineErrorConnector(device[1], device[2])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((device[2], 25000))
+        #s.sendall(b"Hello, world")
+        data = s.recv(1024)
 
-users = []  # To store their name
-sort = []  # To store their socket
+    print(f"Received {data!r}")
 
-
-def listen_user(user):
-    print('Listening user')
-    sort.append(user)  # Store their socket in sort[]
-    user.send('Name'.encode('utf-8'))  # send 'Name' to clients
-    name = user.recv(1024).decode('utf-8')  # Receive their name
-    users.append(name)  # Store their name in user[]
-
-    while True:
-        data = user.recv(1024).decode('utf-8')
-        print(f'{name} sent {data}')
-
-        for i in sort:  # Send received messages to clients
-            if (i != server and i != user):  # Filter server and message sender. Send message except them.
-                i.sendall(f'{name} > {data}'.encode('utf-8'))
-
-
-def start_server():
-    while True:
-        user_socket, addr = server.accept()
-        potok_info = threading.Thread(target=listen_user, args=(user_socket,))
-        potok_info.start()
-
-
-if __name__ == '__main__':
-    start_server()
 
 
