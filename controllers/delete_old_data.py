389be9b7a5720327c1f0cdcae0b4e7bc7adf8ff3
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2024-02-07 13:57:06 +0300

    data clean

---
+++ controllers/delete_old_data.py
@@ -0,0 +1,13 @@
+import sys
+import websocket
+sys.path.append('./')
+sys.path.append('/var/www/Telemetry_wmf24/')
+from datetime import datetime, timezone
+from controllers.db.models import WMFSQLDriver
+
+db_conn = WMFSQLDriver()
+
+devices = db_conn.get_devices()
+time_low_limit = datetime.fromtimestamp(int(datetime.now().timestamp() - 604800))
+db_conn.clean_data_tables(time_low_limit)
+
