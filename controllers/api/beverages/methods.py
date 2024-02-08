92cccc8051bf2501870d845f06e155a75bd3b63c
Dmitry <dimonstalkers@mail.ru>
2024-02-08 14:18:21 +0300

    flask fix

--- controllers/api/beverages/methods.py
+++ controllers/api/beverages/methods.py
@@ -161,6 +161,7 @@ def Take_Create_Beverage_Statistics(last_send, device):
                                                 count_in_base += 1
                                             print("Неизвестный рецепт")
                                 else:
+                                    print({count_of_real_pours,count_in_base })
                                     while count_of_real_pours != count_in_base:
                                         db_conn.initPours(device[1], recipe_number, pours_detected_in_base[0][3], recipe_size, pours_detected_in_base[0][4], pours_detected_in_base[0][5], pours_detected_in_base[0][6], pours_detected_in_base[0][7], pours_detected_in_base[0][8], time_to_form)
                                         count_in_base += 1
