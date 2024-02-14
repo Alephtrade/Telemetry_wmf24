67da0d87f755bb7d0aaf457ce30db940381f2ddd
Alephtrade <123379384+Alephtrade@users.noreply.github.com>
2024-02-14 08:29:55 +0300

    data clean

--- controllers/machines_optimizer.py
+++ controllers/machines_optimizer.py
@@ -1,5 +1,6 @@
 import sys
 import websocket
+import requests
 sys.path.append('./')
 sys.path.append('/var/www/Telemetry_wmf24/')
 from datetime import datetime, timezone
@@ -7,16 +8,31 @@ from controllers.db.models import WMFSQLDriver
 
 db_conn = WMFSQLDriver()
 
+
+def status_send_anyway(status_machine, device_aleph_id):
+    data = {'aleph_id': device_aleph_id, "status": status_machine}
+    url = "https://backend.wmf24.ru/api/km_status"
+    headers = {
+        'Content-Type': 'application/json',
+        'Serverkey': db_conn.get_encrpt_key()[0]
+    }
+    response = requests.request("POST", url, headers=headers, json=data)
+
+
 devices = db_conn.get_devices()
 for device in devices:
     WS_URL = f'ws://{device[2]}:25000/'
     try:
         ws = websocket.create_connection(WS_URL, timeout=5)
+        status = 1
     except Exception:
         ws = False
+        status = 0
+    status_send_anyway(status, device[2])
     if ws == False:
         db_conn.reset_ips(device[1])
-    if int(datetime.strptime(device[4], '%Y-%m-%d %H:%M:%S').timestamp()) + 2419200 < int(datetime.now().timestamp()):
+    if int(datetime.strptime(device[4], '%Y-%m-%d %H:%M:%S').timestamp()) + 2419200 < int(
+            datetime.now().timestamp()):
         db_conn.delete_device(device[0])
     if int(datetime.strptime(device[4], '%Y-%m-%d %H:%M:%S').timestamp()) + 43200 < int(datetime.now().timestamp()):
         db_conn.reset_ips(device[0])
