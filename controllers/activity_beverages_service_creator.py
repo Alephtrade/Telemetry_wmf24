dbd8d028fbd18134b9084f2d846ab4c1068eb1a8
IT <it@alephtrade.com>
2024-02-28 11:34:09 +0300

    drop device

--- controllers/activity_beverages_service_creator.py
+++ controllers/activity_beverages_service_creator.py
@@ -86,7 +86,7 @@ def get_main_clean_stat(device):
             disconnect_end_time = time_now
         print({disconnect_end_time, disconnect_start_time})
         disconnect_time = disconnect_end_time - disconnect_start_time
-        disconnect_time = timedelta_int(disconnect_time)
+        #disconnect_time = timedelta_int(disconnect_time)
         total_disconnect_time += disconnect_time
         print({"db_id", disconnect_rec_id})
         print({"time", disconnect_time})
