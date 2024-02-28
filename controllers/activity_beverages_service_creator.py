96f67024fa41bf53003cbd495f283a8f7a320e44
IT <it@alephtrade.com>
2024-02-28 15:39:21 +0300

    drop device

--- controllers/activity_beverages_service_creator.py
+++ controllers/activity_beverages_service_creator.py
@@ -38,7 +38,7 @@ def get_main_clean_stat(device):
     wmf_error_count = 0
     disconnect_count = 0
 
-    print({unsent_disconnect_records, unsent_records})
+    print(unsent_disconnect_records, unsent_records)
     for disconnect_rec_id, disconnect_error_code, disconnect_start_time, disconnect_end_time in unsent_disconnect_records:
         for rec_id, error_code, error_start_time, error_end_time in unsent_records:
             if error_start_time is not None:
