17bf37e47b5ca3dfd7d7a1394a15e4ebecf4bff7
Dmitry <dimonstalkers@mail.ru>
2024-03-20 12:28:53 +0300

    snmp test

--- controllers/test.py
+++ controllers/test.py
@@ -20,7 +20,8 @@ def walk(host, oid):
             break
         else:
             for varBind in varBinds:
-                print(varBind)
+                a.append(varBind)
+    print(1)
 
 
 walk('10.8.0.6', '1.3.6.1.4.1.2021.8.1.101')
