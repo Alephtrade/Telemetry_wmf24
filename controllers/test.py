8e9a73df8a3443dfaf1f508aad5b8d3a5f0371cc
Dmitry <dimonstalkers@mail.ru>
2024-03-20 14:09:26 +0300

    snmp test

--- controllers/test.py
+++ controllers/test.py
@@ -1,10 +1,22 @@
 import sys
-
+import requests
 from pysnmp.hlapi import *
+from controllers.db.models import WMFSQLDriver
+import json
 
+db_conn = WMFSQLDriver()
 
-def walk(host, oid):
-    a = []
+def walk(aleph_id, host, oid):
+    a = {}
+    a["aleph_id"] = aleph_id
+    a["modelName"] = ""
+    a["serialNum"] = ""
+    a["version"] = ""
+    a["simCcid"] = ""
+    a["signalPower"] = ""
+    a["status"] = ""
+    a["connectType"] = ""
+    i = 1
     for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(SnmpEngine(),
                                                                         CommunityData('public'),
                                                                         UdpTransportTarget((host, 161)), ContextData(),
@@ -20,8 +32,18 @@ def walk(host, oid):
             break
         else:
             for varBind in varBinds:
-                a.append(varBinds[0][1].prettyPrint())
+                a[i] = varBinds[0][1].prettyPrint()
+                i = i + 1
+    url = "https://backend.wmf24.ru/api/sim_informer"
+    headers = {
+        'Content-Type': 'application/json',
+        'Serverkey': db_conn.get_encrpt_key()[0]
+    }
+    #response = requests.request("POST", url, headers=headers, data=json.dumps(a))
+    # print(response["aleph_id"])
     return a
 
 
-print(walk('10.8.0.6', '1.3.6.1.4.1.2021.8.1.101'))
+devices = db_conn.get_devices()
+for device in devices:
+    print(walk(device[1], device[2], '1.3.6.1.4.1.2021.8.1.101'))
