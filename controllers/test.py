e280c3969b8bc453ae04c9d8ee95cdfd6e2acd6c
Dmitry <dimonstalkers@mail.ru>
2024-03-20 14:16:50 +0300

    snmp test

--- controllers/test.py
+++ controllers/test.py
@@ -18,6 +18,7 @@ def walk(aleph_id, host, oid):
     a["signalPower"] = ""
     a["status"] = ""
     a["connectType"] = ""
+    i = 1
     for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(SnmpEngine(),
                                                                         CommunityData('public'),
                                                                         UdpTransportTarget((host, 161)), ContextData(),
@@ -33,8 +34,8 @@ def walk(aleph_id, host, oid):
             break
         else:
             for varBind in varBinds:
-                for key in a.keys():
-                    a[key] = varBinds[0][1].prettyPrint()
+                a[a.keys()[i]] = varBinds[0][1].prettyPrint()
+                i = i + 1
     url = "https://backend.wmf24.ru/api/sim_informer"
     headers = {
         'Content-Type': 'application/json',
