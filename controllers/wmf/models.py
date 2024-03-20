fbd955cdf93f4586785fc96572381049f5efab54
Dmitry <dimonstalkers@mail.ru>
2024-03-20 15:49:30 +0300

    snmp test

--- controllers/wmf/models.py
+++ controllers/wmf/models.py
@@ -92,7 +92,7 @@ class WMFMachineErrorConnector:
                     if error_code in self.current_errors:
                         self.current_errors.remove(error_code)
             if data.get("function") == 'startPushDispensingFinished' or data.get("function") == 'getRecipeComposition':
-                #print(data)
+                print(data)
                 #print(data.get("RecipeNumber"))
                 recipe_number = data.get("RecipeNumber")
                 cup_size = data.get("CupSize")
